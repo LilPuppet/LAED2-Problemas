f03426d09fd038653c5be466ad641b0f74c1d9f2
Lavínia Dantas <100712081+LilPuppet@users.noreply.github.com>
2022-08-04 09:23:25 -0300

    Update Problema1.cpp

--- Problema1.cpp
+++ Problema1.cpp
@@ -4,13 +4,16 @@ int aaa(int n);
 
 int main(){
     clock_t t1;
+    int n;
     t1 = clock();
-    printf("%i\n", aaa(100));
+    printf("Defina o número:");
+    scanf("%i", &n);
+    printf("%i\n", funcaoDemorada(n));
     t1 = clock()-t1;
     printf("%f\n", (float)t1/CLOCKS_PER_SEC);
 }
 
-int aaa(int n){
+int funcaoDemorada(int n){
     int soma=0;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
