cc428f007317ccd3b76b0a52e4002749c5d4901d
Lavínia Dantas <100712081+LilPuppet@users.noreply.github.com>
2022-08-11 09:02:24 -0300

    Update Problema6.cpp
    Recursão adicionada

--- Problema6.cpp
+++ Problema6.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 
+void VetorAoContrario(int n[], int qtd, int ini);
 int main(){
     int qtd;
     printf("Insira o tamanho do vetor:");
@@ -8,11 +8,21 @@ int main(){
     int v[qtd];
     printf("\nInsira os valores do vetor:\n");
     for (int i = 0; i <qtd; i++){
-        scanf("%i", &v[qtd-i-1]);
+        scanf("%i", &v[i]);
     }
-
+    VetorAoContrario(v, qtd-1, 0);
     for (int i = 0; i <qtd; i++){
-        printf("%i\t", v[i]);    
+        printf("%i\t", v[i]);
     }
+    
+}
 
+void VetorAoContrario(int n[], int qtd, int ini) {
+  int x;
+  if (qtd > ini){
+    x = n[ini];
+    n[ini]=n[qtd];
+    n[qtd]= x;
+    VetorAoContrario(n, qtd-1, ini+1); 
+  }
 }
