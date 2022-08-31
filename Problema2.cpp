7067a9cbddc341f2343c603e3849d0efc2dc439b
Francisco Afonso <72872359+afonsojrm@users.noreply.github.com>
2022-08-31 20:29:54 -0300

    Create Problema2.cpp

---
+++ Problema2.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+        
+ float *sequencias_geradas (size_t n) {
+    size_t i;
+    srand (time(NULL));
+    float *res = (float*) malloc (n*sizeof(float));
+    for (i = 0; i < n ; i ++) {
+    	res [i] = rand()%1000;
+    }
+    return res ;
+}
+
+
+int main () {
+    setlocale(LC_ALL,"");   
+    printf (" Gerador de sequencias aleatorias: \n");
+    clock_t t1;
+    t1 = clock();
+    int n = 100, i;
+	float*res = sequencias_geradas (n);
+    int media = 0;
+    for (i = 0; i < n ; i ++) {
+    printf ("res [%i] %.2f\n", i, res[i]);
+    media = media + res[i];
+    }
+    t1 = clock () - t1;
+	printf("\n tempo final %f 's\n", (((float)t1)/CLOCKS_PER_SEC));
+    printf("\n media resultante: %i\n", media/n);
+
+    return 0;
+ }
