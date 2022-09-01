88bb48e9d424ee7cf76ee5e3ed80b99510f2e046
Lavínia Dantas <100712081+LilPuppet@users.noreply.github.com>
2022-09-01 04:04:48 -0300

    Update Problema1.cpp
    Comentários adicionados.

--- Problema1.cpp
+++ Problema1.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <time.h>
-int funcaoDemorada(int n);
+int funcaoDemorada(int n); //chama-se a função para ser executada no main
 
 int main(){
-    clock_t t1;
-    int n;
-    t1 = clock();
-    printf("Defina o numero:");
+    clock_t t1;//cria clock
+    int n; //cria número que será usado na função
+    t1 = clock(); //define clock como tempo atual da máquina
+    printf("Defina o numero:"); //pede o número e lê
     scanf("%i", &n);
-    printf("%i\n", funcaoDemorada(n));
-    t1 = clock()-t1;
-    printf("%f\n", (float)t1/CLOCKS_PER_SEC);
+    printf("%i\n", funcaoDemorada(n));//executa a função
+    t1 = clock()-t1; //clock se torna o tempo atual menos o marcado anteriormente
+    printf("%f\n", (float)t1/CLOCKS_PER_SEC);//imprime o tempo de execução da função
 }
 
 int funcaoDemorada(int n){
     int soma=0;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            for (int k = 0; k < n; k++){
-                for (int l = 0; l < n; l++){
-                    soma = i+j+k+l;
+    for (int i = 0; i < n; i++){//vai repetir de 0 a n
+        for (int j = 0; j < n; j++){//dentro de 0 a n, vai repetir novamente de 0 a n
+            for (int k = 0; k < n; k++){//dentro dos dois 0 a n, repete mais uma vez de 0 a n
+                for (int l = 0; l < n; l++){//dentro dos três 0 a n, repete mais uma vez de 0 a n
+                    soma = i+j+k+l; //soma os números dos contadores em cada repetição, quanto maior o número, mais demorado
                 }
             }
         }
     }
-    return soma;
+    return soma;//retorna a soma
 }
